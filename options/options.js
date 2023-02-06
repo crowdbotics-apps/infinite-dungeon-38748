1f5c60cc9e201edf5062d2f014a40077c79c5578
Crowdbotics <team@crowdbotics.com>
2023-02-06 18:40:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "infinite-dungeon-38748",
+  url: "https://infinite-dungeon-38748.botics.co",
+  api: "https://infinite-dungeon-38748.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
